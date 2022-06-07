ae24598b8febfa0b5f760ad6a6ae456c106be958
Tyler-RIx <rixt@limestone.on.ca>
2022-06-07 11:00:25 -0400

    Images and code for animation added

--- Culminating Project/Orc.java
+++ Culminating Project/Orc.java
@@ -1,19 +1,103 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Orc here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Animations using arrays of images.
  */
 public class Orc extends Actor
 {
     /**
-     * Act - do whatever the Orc wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Movement variables.
      */
-    public void act() 
+    boolean isFacingRight = true;
+    
+    /**
+     * Animation variables.
+     */
+    int animationInterval = 10;  // Number of frames between animation images.
+    int frameCounter = 0;       // Frame counter. For animations.
+    
+    /**
+     * Arrays for animation images.
+     */
+    GreenfootImage[] imagesWalkRight;
+    GreenfootImage[] imagesWalkLeft;
+    GreenfootImage[] imagesIdleRight;
+    GreenfootImage[] imagesIdleLeft;
+    
+    
+    public Orc()
     {
-        // Add your action code here.
-    }    
+        loadImages();
+    }
+    
+    
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("right"))
+        {
+            isFacingRight = true;
+            animate(imagesWalkRight);
+        }
+        else if (Greenfoot.isKeyDown("left"))
+        {
+            isFacingRight = false;
+            animate(imagesWalkLeft);
+        }
+        else if (isFacingRight == true)
+        {
+            animate(imagesIdleRight);
+        }
+        else
+        {
+            animate(imagesIdleLeft);
+        }
+    }
+    
+    
+    /**
+     * Animates using the specified images.
+     */
+    void animate(GreenfootImage[] images)
+    {
+        if (frameCounter >= images.length * animationInterval)  // Greater or equal (>=) takes care of animations with different number of images.
+        {
+            frameCounter = 0;
+        }
+        
+        if (frameCounter % animationInterval == 0)  // If it's time to switch to next animation image.
+        {
+            setImage(images[frameCounter/animationInterval]);
+        }
+        
+        frameCounter++;
+    }
+    
+    
+    /**
+     * Loads images used for animations.
+     */
+    void loadImages()
+    {
+        imagesWalkRight = new GreenfootImage[6];
+        imagesWalkLeft = new GreenfootImage[6];
+        imagesIdleRight = new GreenfootImage[4];
+        imagesIdleLeft = new GreenfootImage[4];
+        
+        for (int i = 0; i < imagesWalkRight.length; i++)
+        {
+            // Assuming the image files are tile0.png, tile1.png, etc.
+            String imagePath = "images/Orc_Walk" + i + ".png";
+            imagesWalkRight[i] = new GreenfootImage(imagePath);
+            imagesWalkLeft[i] = new GreenfootImage(imagePath);
+            imagesWalkLeft[i].mirrorHorizontally();
+        }
+        for (int i = 0; i < imagesIdleRight.length; i++)
+        {
+            // Again, assuming the image files are tile0.png, tile1.png, etc.
+            String imagePath = "3 SteamMan/Idle/tile" + i + ".png";
+            imagesIdleRight[i] = new GreenfootImage(imagePath);
+            imagesIdleLeft[i] = new GreenfootImage(imagePath);
+            imagesIdleLeft[i].mirrorHorizontally();
+        }
+    }
 }
