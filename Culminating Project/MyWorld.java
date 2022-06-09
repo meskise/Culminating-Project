4f59cf532eaf3ef6389b08e3cdd3d68f30cdd885
meskise <meskise@limestone.on.ca>
2022-06-09 09:11:50 -0400

    Added Temporary Platforms & Player movement
    added basic movement and gravity for player and temporary platforms to test

--- Culminating Project/MyWorld.java
+++ Culminating Project/MyWorld.java
@@ -17,5 +17,43 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        PlayerOne player = new PlayerOne();
+        addObject(player,20,330);
+        
+        Platform platform = new Platform();
+        addObject(platform,20,380);
+        Platform platform2 = new Platform();
+        addObject(platform2,60,380);
+        Platform platform3 = new Platform();
+        addObject(platform3,100,380);
+        Platform platform4 = new Platform();
+        addObject(platform4,140,380);
+        Platform platform5 = new Platform();
+        addObject(platform5,180,380);
+        Platform platform6 = new Platform();
+        addObject(platform6,300,380);
+        Platform platform7 = new Platform();
+        addObject(platform7,340,380);
+        Platform platform8 = new Platform();
+        addObject(platform8,380,380);
+        Platform platform9 = new Platform();
+        addObject(platform9,420,380);
+        Platform platform10 = new Platform();
+        addObject(platform10,460,380);
+        Platform platform11 = new Platform();
+        addObject(platform11,500,380);
+        Platform platform12 = new Platform();
+        addObject(platform12,540,380);
+        Platform platform13 = new Platform();
+        addObject(platform13,580,380);
     }
 }
---
+++ Culminating Project/PlayerOne.java
@@ -0,0 +1,202 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PlayerOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlayerOne extends Actor
+{
+    // Check if facing right or is in air.
+    boolean isFacingRight;
+    boolean isInAir;
+    
+    // Movement variables.
+    double deltaX = 0;
+    double deltaY = 0;
+    
+    // Gravity.
+    double g = 0.8;
+    
+    /** 
+     * Scale and load all images.
+     */
+    public PlayerOne()
+    {
+        GreenfootImage image = getImage();
+        image.scale(35,45);
+        setImage(image);
+    }
+    
+    /**
+     * Act for all voids.
+     */
+    public void act() 
+    {
+        movementKeys();
+        applyGravity();
+        collisonCheck();
+    }
+    
+    /**
+     * Basic movement.
+     */
+    public void movementKeys()
+    {
+        // Movement keys.
+        if (Greenfoot.isKeyDown("a"))
+        {
+            deltaX = - 3.5;
+        }
+        else if (Greenfoot.isKeyDown("d"))
+        {
+            deltaX = 3.5;
+        }
+        else
+        {
+            deltaX = 0;
+        }
+        
+        if (Greenfoot.isKeyDown("space"))
+        {
+            // Only allow jump when touching Ground / Platform classes.
+            if (isTouching(Platform.class))
+            {
+                // Jump. 
+                deltaY = -14;
+            }
+        }
+        
+        
+        // Allows movement.
+        setLocation(getX() + (int)deltaX, getY() + (int)deltaY);
+        
+        // If deltaX is greater than 0 then facing right is true.
+        if (deltaX > 0)
+        {
+            isFacingRight = true;
+        }
+        // If deltaX is less than 0 then facing right is false.
+        else if (deltaX < 0)
+        {
+            isFacingRight = false;
+        }
+        
+    }
+    
+    /**
+     * Move to the top of an object when called.
+     */
+    public void moveOnTopOfObject(Actor object)
+    {
+        // Allows player to be adjusted to top of the platforms to prevent bugs.
+        int height = getImage().getHeight();
+        int objectHeight = object.getImage().getHeight();
+        
+        setLocation(getX(), object.getY() - objectHeight / 2 - height / 2 + 1);
+    }
+    
+    /**
+     * Move to the bottom of an object when called.
+     */
+    public void moveOnBottomOfObject(Actor object)
+    {
+        // Allows player to be adjusted to top of the platforms to prevent bugs.
+        int height = getImage().getHeight();
+        int objectHeight = object.getImage().getHeight();
+        
+        setLocation(getX(), object.getY() + objectHeight / 2 + height / 2 + 1);
+    }
+    
+    /**
+     * Stop right on object when called.
+     */
+    public void stopOnRightObject(Actor object)
+    {
+        int width = getImage().getWidth();
+        int objectWidth = object.getImage().getWidth();
+        
+        setLocation(object.getX() + objectWidth /2 + width /2, getY());
+    }
+    
+    /**
+     * Stop left on object when called.
+     */
+    public void stopOnLeftObject(Actor object)
+    {
+        int width = getImage().getWidth();
+        int objectWidth = object.getImage().getWidth();
+        
+        setLocation(object.getX() - objectWidth /2 - width /2, getY());
+    }
+    
+    /**
+     * Apply gravity to player.
+     */
+    public void applyGravity()
+    {
+        // Height of the character, which is just height of the current image.
+        int height = getImage().getHeight();
+        int width = getImage().getWidth();
+        
+        Actor platform = getOneObjectAtOffset(0, height / 2, Platform.class);
+        if (platform != null) // If there is a platform below.
+        {
+            deltaY = 0; // Don't apply gravity
+            isInAir = false;
+          
+            moveOnTopOfObject(platform); // Adjust position to just touching platform.
+        }
+        else // No platform below.
+        {
+            
+            isInAir = true;
+        }
+        
+        platform = getOneObjectAtOffset(0, - height / 2, Platform.class);
+        if (platform != null) // If there is a platform above.
+        {
+            isInAir = true;
+            deltaY = 0;
+          
+            moveOnBottomOfObject(platform); // Adjust position to just touching platform.
+        }
+        
+        if (isInAir == true)
+        {
+            deltaY = deltaY + g; // Apply gravity
+        }
+    }
+    
+    /**
+     * Check for collisons.
+     */
+    public void collisonCheck()
+    {
+        int height = getImage().getHeight();
+        int width = getImage().getWidth();
+        
+        Actor platform = getOneObjectAtOffset( - width /2, 0 ,Platform.class);
+        if (platform != null)
+        {
+            deltaX = 0;
+            
+            stopOnRightObject(platform);
+        }
+        
+        platform = getOneObjectAtOffset( width /2, 0, Platform.class);
+        if (platform != null)
+        {
+            deltaX = 0;
+            
+            stopOnLeftObject(platform);
+        }
+        
+        if (getY() >= 395)
+        {
+            //Greenfoot.setWorld(new GameOver());
+            Greenfoot.stop();
+        }
+    }
+}
