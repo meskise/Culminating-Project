dc13f48e1cb7d025750af21bd6c54d096068866c
Tyler-RIx <rixt@limestone.on.ca>
2022-06-10 10:30:15 -0400

    Added spikes
    Added to paint order
    Added to world for testing

---
+++ Culminating Project/Spike.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spike here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spike extends Actor
+{
+    public Spike()
+    {
+        GreenfootImage image = getImage();
+        image.scale(60, 30);
+        setImage(image);
+    }
+    /**
+     * Act - do whatever the Spike wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
